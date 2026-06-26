eab5f388aa8f5fe6df504238679a5d2de6dcbf1f
KanishkaSinghal1 <kanishkafrontend@gmail.com>
2026-06-26 18:46:15 +0530

    Features: Added CRUD APIs

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const app = express(); // create an express app
 app.use(express.json());
 
 import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js"
 //import postRouter from "./routes/post.route.js";
 //route declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 //app.use("/api/v1/posts", postRouter);
 // example route: http://localhost:4000/api/v1/users/register
 export default app;
